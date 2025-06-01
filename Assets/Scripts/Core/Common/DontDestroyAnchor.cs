f5e8bff624595d973f52e2ad8a7612c566138858
Kursat Sahin <a.kursat.sahin@gmail.com>
2025-06-01 03:02:14 +0300

    BaseEnv was created and added to Splash but thats nasty/cheap solution

---
+++ Assets/Scripts/Core/Common/DontDestroyAnchor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Match3d.Core.Common
+{
+    public class DontDestroyAnchor : MonoBehaviour
+    {
+        void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
